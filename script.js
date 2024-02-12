aa1443feb7549dfefed6630e5f609533fc80f7fa
Juani-tech <jperezdichiazza@gmail.com>
2024-02-11 22:17:08 -0300

    validations for passwords, more css

---
+++ script.js
@@ -0,0 +1,12 @@
+let password = document.getElementById('password');
+let confirm_password = document.getElementById('confirm-password');
+
+function validatePassword(){
+    if(password.value !== confirm_password.value){
+        confirm_password.setCustomValidity('Passwords do not match');
+    } else {
+        confirm_password.setCustomValidity('');
+    }
+}
+confirm_password.oninput = validatePassword;
+password.oninput = validatePassword;
