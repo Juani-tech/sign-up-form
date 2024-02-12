d2e541cc568b2e719aed437a7a52c722dc6f819a
Juani-tech <jperezdichiazza@gmail.com>
2024-02-11 22:21:03 -0300

    fix password check

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ let confirm_password = document.getElementById('confirm-password');
 function validatePassword(){
     if(password.value !== confirm_password.value){
         confirm_password.setCustomValidity('Passwords do not match');
+        password.setCustomValidity('Passwords do not match');
     } else {
         confirm_password.setCustomValidity('');
+        password.setCustomValidity('');
     }
 }
 confirm_password.oninput = validatePassword;
